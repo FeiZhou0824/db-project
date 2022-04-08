d088c4fa4ee86b56a3ecdecb1e95a8c9d9e756ec
FeiZhou <sophia08241@gmail.com>
2022-04-07 22:57:12 -0400

    HomeWork update

--- Queries/homeWork.sql
+++ Queries/homeWork.sql
@@ -171,16 +171,6 @@ WHERE JOB_TITLE = 'Programmer';
 
 /*
  Employee Table
-
-
-
-
-    2.2  Display All Employees FIRST_NAME , JOB_ID , SALARY
-        if the JOB_ID ends with REP BUT not equal SA_REP
-        ORDER the result by SALARY DESC
-
-    2.3 Display All Employees FIRST_NAME if second Character is a
-        ORDER the result by LENGTH of FIRST_NAME
  */
 
 
@@ -190,61 +180,79 @@ SELECT FIRST_NAME,JOB_ID,SALARY
 FROM EMPLOYEES
 WHERE JOB_ID LIKE '%ACCOUNT';
 
+-- 2.2  Display All Employees FIRST_NAME , JOB_ID , SALARY
+--if the JOB_ID ends with REP BUT not equal SA_REP
+  --      ORDER the result by SALARY DESC
 
+SELECT FIRST_NAME,JOB_ID,SALARY
+FROM EMPLOYEES
+WHERE JOB_ID LIKE '%REP' AND JOB_ID != 'SA_REP'
+ORDER BY SALARY DESC;
 
+--2.3 Display All Employees FIRST_NAME if second Character is a
+  --      ORDER the result by LENGTH of FIRST_NAME
+
+SELECT FIRST_NAME
+FROM EMPLOYEES
+WHERE FIRST_NAME LIKE '_a%'
+ORDER BY LENGTH(FIRST_NAME);
 
 /*
  Department Table
-
-
-
-    2.5 Display All Departments with DEPARTMENT_NAME has more than 1 word (Contains space)
-
  */
 
 
 --2.4 Display All Departments with DEPARTMENT_NAME Contains IT
 ----  exclude the exact match department name IT
-
 SELECT *
 FROM DEPARTMENTS
 WHERE DEPARTMENT_NAME LIKE'%IT%';
+
+-- 2.5 Display All Departments with DEPARTMENT_NAME has more than 1 word (Contains space)
+SELECT DEPARTMENT_NAME
+FROM DEPARTMENTS
+WHERE  DEPARTMENT_NAME LIKE '% %';
 /*
  Jobs Table
 
-    2.6 Display All JOB_TITLE that contains Manager
-
-    2.7 Display All JOB_TITLE that starts with A , sort in descending order
-
  */
 
+ --  2.6 Display All JOB_TITLE that contains Manager
+SELECT JOB_TITLE
+FROM JOBS
+WHERE JOB_TITLE LIKE '%Manager';
 
+-- 2.7 Display All JOB_TITLE that starts with A , sort in descending order
+SELECT JOB_TITLE
+FROM JOBS
+WHERE JOB_TITLE LIKE 'A%'
+ORDER BY JOB_TITLE;
 
 
 /*Employees Table
 
-    3.1  Display Employees FIRST_NAME , LAST_NAME , FULL_NAME, LENTH OF FULL_NAME
-         if the LENTH OF FULL_NAME is more than 12 , SORT FIRST_NAME ASC
-
-    3.2 Display the Employee Count for employees make more than 10000
-
-    3.2 Display the Employee Count for employees that has Manager_ID 108
-
-    3.3 Display the Employee Count for EACH Manager_ID
-
-    3.4 Display the Employee Count for EACH FIRST_NAME (yeah and observe what you got)
-            Filter the grouped result to only display if the count is more than 1
-
-    3.5 Display the Sum of All Salary for  employees with JOB_ID Contains Clerk
-
-    3.6 Display the Max Salary for  employees with JOB_ID contains SA
-
-    3.7 Display the AVERAGE Salary for employees with JOB_ID ends with REP
-
-    3.8 Display the Average Salary for Each Department_ID
-         if the Average Salary is from 5000- 10000
-
  */
+ -- 3.1  Display Employees FIRST_NAME , LAST_NAME , FULL_NAME, LENTH OF FULL_NAME
+--     if the LENTH OF FULL_NAME is more than 12 , SORT FIRST_NAME ASC
+SELECT FIRST_NAME,LAST_NAME,(FIRST_NAME ||' '|| LAST_NAME) AS FULL_NAME
+FROM EMPLOYEES
+WHERE LENGTH (FIRST_NAME || LAST_NAME) >12;
+
+-- 3.2 Display the Employee Count for employees make more than 10000
+SELECT COUNT(*)
+FROM EMPLOYEES
+WHERE SALARY >10000;
+
+--3.2 Display the Employee Count for employees that has Manager_ID 108
+SELECT COUNT(*)
+FROM EMPLOYEES
+    WHERE MANAGER_ID LIKE '%108%';
+
+--3.3 Display the Employee Count for EACH Manager_ID
+SELECT COUNT(*),MANAGER_ID
+FROM EMPLOYEES
+GROUP BY MANAGER_ID;
+
 
 --  3.4 Display the Employee Count for EACH FIRST_NAME (yeah and observe what you got)
 --Filter the grouped result to only display if the count is more than 1
@@ -254,9 +262,40 @@ FROM EMPLOYEES
 GROUP BY FIRST_NAME
 HAVING COUNT(*) >1;
 
+-- 3.5 Display the Sum of All Salary for  employees with JOB_ID Contains Clerk
+SELECT JOB_ID,
+       SUM(SALARY)
+FROM EMPLOYEES
+group by JOB_ID
+HAVING JOB_ID LIKE '%CLERK%';
+
+--3.6 Display the Max Salary for  employees with JOB_ID contains SA
+SELECT JOB_ID,
+       MAX(SALARY)
+FROM EMPLOYEES
+WHERE JOB_ID LIKE '%SA%'
+GROUP BY JOB_ID;
+--HAVING JOB_ID LIKE '%SA%';
+
+--3.7 Display the AVERAGE Salary for employees with JOB_ID ends with REP
+SELECT JOB_ID,
+       AVG(SALARY)
+FROM EMPLOYEES
+WHERE JOB_ID LIKE '%REP'
+GROUP BY JOB_ID;
+
+-- 3.8 Display the Average Salary for Each Department_ID
+--if the Average Salary is from 5000- 10000
+SELECT DEPARTMENT_ID,
+      -- AVG(SALARY),
+       ROUND(AVG(SALARY),3)
+FROM EMPLOYEES
+GROUP BY DEPARTMENT_ID
+HAVING AVG(SALARY) BETWEEN 5000 AND 10000;
+
 /*Departments Table
 
-   3.9 Display the Count of Departments in LOCATION_ID 1700
+
 
    3.10 Display the Count of Departments in EACH LOCATION_ID
 
@@ -265,5 +304,50 @@ HAVING COUNT(*) >1;
    3.12 Display the Count of Departments that contains IT in DEPARTMENT_NAME
 
  */
+ --3.9 Display the Count of Departments in LOCATION_ID 1700
+ SELECT LOCATION_ID,COUNT(*)
+FROM DEPARTMENTS
+WHERE LOCATION_ID = 1700
+GROUP BY LOCATION_ID;
 
 
+/*
+ Locations Table
+
+   3.13 Display the count of Location that does not have STATE_PROVINCE
+
+   3.14 Display the count of Location in US, CA, IN
+
+   3.15 Display the count of Location in EACH COUNTRY (COUNTRY_ID)
+
+   3.16 Display the count of Location for those location Contains st, Street in STREET_ADDRESS
+
+   3.15 Display the count of Location in EACH COUNTRY (COUNTRY_ID)
+         if the count is less than 3
+
+Countries Table
+
+   3.16 Display the count of Countries
+
+   3.17 Display the count of Countries in Region 3
+
+   3.18 Display the count of Countries that has COUNTRY_NAME ended with a
+
+   3.19 Display the count if Countries for each REGION (REGION_ID)
+
+   3.19 Display the count if Countries for each REGION (REGION_ID)
+        if the count is more than 5
+
+
+ Jobs Table
+
+    3.2O Find out MAX(MIN_SALARY) in all JOBS
+
+    3.21 Find out MAX (MAX_SALARY) for JOBS excluding President
+
+    3.22 Display AVG(MAX_SALARY) (this is column name) for JOBS that has Manager in TITLE
+
+* /
+
+
+ */
